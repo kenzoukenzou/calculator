dc8618b3ef10dd1cd5b2409afa6f80a60113dbba
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-07-28 08:59:39 +0900

    add: basic UI (数字ボタン配置)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">MC</div>
+        <div className="col">MR</div>
+        <div className="col">M-</div>
+        <div className="col">M+</div>
+      </div>
+      <div className="row">
+        <div className="col">+/-</div>
+        <div className="col">7</div>
+        <div className="col">8</div>
+        <div className="col">9</div>
+      </div>
+      <div className="row">
+        <div className="col">%</div>
+        <div className="col">4</div>
+        <div className="col">5</div>
+        <div className="col">6</div>
+      </div>
+      <div className="row">
+        <div className="col">C</div>
+        <div className="col">1</div>
+        <div className="col">2</div>
+        <div className="col">3</div>
+      </div>
+      <div className="row">
+        <div className="col">AC</div>
+        <div className="col">0</div>
+        <div className="col">00</div>
+        <div className="col">・</div>
+      </div>
     </div>
   );
 }
