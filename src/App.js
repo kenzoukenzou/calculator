bce729d21ed6715b34c2b2b42d8b6ed8bfa45381
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-07-28 15:00:56 +0900

    fix: calculator UI (ボタンの色等修正)

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import React, { Component } from "react";
 class App extends Component {
   state = {
     calc_result: "",
-    display_result: "",
+    display_result: 0,
     memory: 0
   };
 
   // 四則演算
   calc = cmd => {
-    if (cmd == "=") {
+    if (cmd === "=") {
       this.setState({
         calc_result: eval(this.state.calc_result)
       });
-    } else if (cmd == "AC") {
+    } else if (cmd === "AC") {
       this.setState({
         calc_result: ""
       });
@@ -26,20 +26,20 @@ class App extends Component {
 
   // メモリー機能
   memory = cmd => {
-    if (cmd == "M+") {
+    if (cmd === "M+") {
       this.setState({
         memory: this.state.memory + eval(this.state.calc_result),
         calc_result: "",
         display_result: eval(this.state.calc_result)
       });
-    } else if (cmd == "M-") {
+    } else if (cmd === "M-") {
       this.setState({
         memory: this.state.memory - eval(this.state.calc_result),
         calc_result: "",
         display_result: eval(this.state.calc_result)
       });
-    } else if (cmd == "MR") {
-    } else if (cmd == "MC") {
+    } else if (cmd === "MR") {
+    } else if (cmd === "MC") {
       this.setState({
         memory: 0
       });
@@ -49,31 +49,42 @@ class App extends Component {
   render() {
     return (
       <div className="container text-center custom-middle">
-        <p>memory: {this.state.memory}</p>
-        <p>result: {this.state.display_result}</p>
-        <input type="text" name="text" value={this.state.calc_result} />
+        <div className="row mb-3">
+          <div className="col font-weight-bold">
+            メモリ: {this.state.memory}
+          </div>
+          <div className="col font-weight-bold">
+            計算結果: {this.state.display_result}
+          </div>
+        </div>
+        <input
+          type="text"
+          name="text"
+          className="form-control mb-2"
+          value={this.state.calc_result}
+        />
         <div className="row">
           <div
-            className="col mr-1 bg-danger text-white"
+            className="col mr-1 btn btn-outline-success"
             onClick={e => this.memory("MC")}
           >
             MC
           </div>
-          <div className="col mr-1 bg-danger text-white">MR</div>
+          <div className="col mr-1 btn btn-outline-success">MR</div>
           <div
-            className="col mr-1 bg-danger text-white"
+            className="col mr-1 btn btn-outline-success"
             onClick={e => this.memory("M-")}
           >
             M-
           </div>
           <div
-            className="col mr-1 bg-danger text-white"
+            className="col mr-1 btn btn-outline-success"
             onClick={e => this.memory("M+")}
           >
             M+
           </div>
           <div
-            className="col mr-1 bg-danger text-white"
+            className="col mr-1 btn btn-success"
             onClick={e => this.calc("*")}
           >
             ×
@@ -81,17 +92,17 @@ class App extends Component {
         </div>
         <div className="row mt-3">
           <div className="col mr-1 text-white" />
-          <div className="col" onClick={e => this.calc("7")}>
+          <div className="btn btn-light col mr-1" onClick={e => this.calc("7")}>
             7
           </div>
-          <div className="col" onClick={e => this.calc("8")}>
+          <div className="btn btn-light col mr-1" onClick={e => this.calc("8")}>
             8
           </div>
-          <div className="col" onClick={e => this.calc("9")}>
+          <div className="btn btn-light col mr-1" onClick={e => this.calc("9")}>
             9
           </div>
           <div
-            className="col mr-1 bg-danger text-white"
+            className="col mr-1 btn btn-success"
             onClick={e => this.calc("+")}
           >
             ＋
@@ -99,35 +110,35 @@ class App extends Component {
         </div>
         <div className="row mt-3">
           <div className="col mr-1 text-white" />
-          <div className="col" onClick={e => this.calc("4")}>
+          <div className="btn btn-light col mr-1" onClick={e => this.calc("4")}>
             4
           </div>
-          <div className="col" onClick={e => this.calc("5")}>
+          <div className="btn btn-light col mr-1" onClick={e => this.calc("5")}>
             5
           </div>
-          <div className="col" onClick={e => this.calc("6")}>
+          <div className="btn btn-light col mr-1" onClick={e => this.calc("6")}>
             6
           </div>
           <div
-            className="col mr-1 bg-danger text-white"
+            className="col mr-1 btn btn-success"
             onClick={e => this.calc("/")}
           >
             ÷
           </div>
         </div>
         <div className="row mt-3">
-          <div className="col mr-1 bg-danger text-white">C</div>
-          <div className="col" onClick={e => this.calc("1")}>
+          <div className="col mr-1 btn btn-success">C</div>
+          <div className="btn btn-light col mr-1" onClick={e => this.calc("1")}>
             1
           </div>
-          <div className="col" onClick={e => this.calc("2")}>
+          <div className="btn btn-light col mr-1" onClick={e => this.calc("2")}>
             2
           </div>
-          <div className="col" onClick={e => this.calc("3")}>
+          <div className="btn btn-light col mr-1" onClick={e => this.calc("3")}>
             3
           </div>
           <div
-            className="col mr-1 bg-danger text-white"
+            className="col mr-1 btn btn-success"
             onClick={e => this.calc("-")}
           >
             -
@@ -135,22 +146,25 @@ class App extends Component {
         </div>
         <div className="row mt-3">
           <div
-            className="col mr-1 bg-danger text-white"
+            className="col mr-1 btn btn-success"
             onClick={e => this.calc("AC")}
           >
             AC
           </div>
-          <div className="col" onClick={e => this.calc("0")}>
+          <div className="btn btn-light col mr-1" onClick={e => this.calc("0")}>
             0
           </div>
-          <div className="col" onClick={e => this.calc("00")}>
+          <div
+            className="btn btn-light col mr-1"
+            onClick={e => this.calc("00")}
+          >
             00
           </div>
-          <div className="col" onClick={e => this.calc(".")}>
+          <div className="btn btn-light col mr-1" onClick={e => this.calc(".")}>
             ・
           </div>
           <div
-            className="col mr-1 bg-danger text-white"
+            className="col mr-1 btn btn-success"
             onClick={e => this.calc("=")}
           >
             =
