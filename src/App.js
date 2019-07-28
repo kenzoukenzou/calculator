b5f833048db41ebe6aee2f9e28f3fb98a72748fc
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-07-28 09:22:00 +0900

    add: calculate button (四則演算のボタン配置)

--- src/App.js
+++ src/App.js
@@ -8,30 +8,35 @@ function App() {
         <div className="col mr-1 bg-danger text-white">MR</div>
         <div className="col mr-1 bg-danger text-white">M-</div>
         <div className="col mr-1 bg-danger text-white">M+</div>
+        <div className="col mr-1 bg-danger text-white">×</div>
       </div>
       <div className="row mt-3">
         <div className="col mr-1 bg-warning text-white">+/-</div>
         <div className="col">7</div>
         <div className="col">8</div>
         <div className="col">9</div>
+        <div className="col mr-1 bg-danger text-white">＋</div>
       </div>
       <div className="row mt-3">
         <div className="col mr-1 bg-warning text-white">%</div>
         <div className="col">4</div>
         <div className="col">5</div>
         <div className="col">6</div>
+        <div className="col mr-1 bg-danger text-white">÷</div>
       </div>
       <div className="row mt-3">
         <div className="col mr-1 bg-danger text-white">C</div>
         <div className="col">1</div>
         <div className="col">2</div>
         <div className="col">3</div>
+        <div className="col mr-1 bg-danger text-white">-</div>
       </div>
       <div className="row mt-3">
         <div className="col mr-1 bg-danger text-white">AC</div>
         <div className="col">0</div>
         <div className="col">00</div>
         <div className="col">・</div>
+        <div className="col mr-1 bg-danger text-white">=</div>
       </div>
     </div>
   );
